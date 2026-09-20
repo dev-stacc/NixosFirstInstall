648994e64f990e32c3e9091bd7529115dedb9eb6
Anastasia <cookiedemons@outlook.com>
2026-09-20 03:38:41 -0400

    chore: wrong dir, mb

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-    description = "Agnos bootstrap hosts";
+    description = "Agnostic bootstrap hosts";
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
@@ -19,7 +19,7 @@
                 inherit specialArgs;
                 modules = [ ./hosts/uefi/default.nix ];
             };
-#remember to change the boot loader grub device to match your system at /hosts/default.nix
+#remember to change the boot loader grub device to match your system at /hosts/legacy/default.nix
             AgnosLEGACY = nixpkgs.lib.nixosSystem {
                 inherit specialArgs;
                 modules = [ ./hosts/legacy/default.nix ];
